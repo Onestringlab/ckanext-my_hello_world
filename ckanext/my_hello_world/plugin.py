ad84ab2aa0db10537bcc8cddda2ac5a5d0473bd8
yulrio <33221012@std.stei.itb.ac.id>
2024-11-17 22:17:06 +0700

    update message

--- ckanext/my_hello_world/plugin.py
+++ ckanext/my_hello_world/plugin.py
@@ -14,41 +14,14 @@
 #             'my_hello_world')
 
 
-# import ckan.plugins as plugins
-# import ckan.plugins.toolkit as toolkit
-
-
-# class MyHelloWorldPlugin(plugins.SingletonPlugin):
-#     plugins.implements(plugins.IConfigurer)
-#     plugins.implements(plugins.IActions)
-
-#     # IConfigurer
-#     def update_config(self, config_):
-#         toolkit.add_template_directory(config_, 'templates')
-#         toolkit.add_public_directory(config_, 'public')
-#         toolkit.add_resource('fanstatic', 'my_hello_world')
-
-#     # IActions
-#     def get_actions(self):
-#         return {
-#             'hello_world': hello_world_action
-#         }
-
-
-# def hello_world_action(context, data_dict):
-#     """
-#     Fungsi ini akan menangani permintaan ke /api/3/action/hello_world
-#     """
-#     return {'message': 'Hello, World! This is a POST request!', 'success': True, 'method': toolkit.request.method}
-
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-from flask import Blueprint, jsonify
+from ckan.plugins.toolkit import h
 
 
 class MyHelloWorldPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.IRoutes)
+    plugins.implements(plugins.IActions)
 
     # IConfigurer
     def update_config(self, config_):
@@ -56,15 +29,16 @@ class MyHelloWorldPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'my_hello_world')
 
-    # IRoutes
-    def get_blueprint(self):
-        blueprint = Blueprint('hello_world', __name__)
+    # IActions
+    def get_actions(self):
+        return {
+            'hello_world': hello_world_action
+        }
 
-        @blueprint.route('/api/3/hello_world', methods=['GET'])
-        def hello_world():
-            return jsonify({
-                'message': 'Hello, World! This is a GET request!',
-                'success': True
-            })
 
-        return blueprint
+def hello_world_action(context, data_dict):
+    """
+    Fungsi ini akan menangani permintaan ke /api/3/action/hello_world
+    """
+    print(h.get_flask_app().url_map)
+    return {'message': 'Hello, World! This is a POST request!', 'success': True, 'method': toolkit.request.method}
