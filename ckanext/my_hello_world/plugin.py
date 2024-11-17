fff4a09aedb33db75c1e915e70fd51badaed647f
yulrio <33221012@std.stei.itb.ac.id>
2024-11-17 18:56:40 +0700

    Update Hello World

--- ckanext/my_hello_world/plugin.py
+++ ckanext/my_hello_world/plugin.py
@@ -1,39 +1,40 @@
+# import ckan.plugins as plugins
+# import ckan.plugins.toolkit as toolkit
+
+
+# class MyHelloWorldPlugin(plugins.SingletonPlugin):
+#     plugins.implements(plugins.IConfigurer)
+
+#     # IConfigurer
+
+#     def update_config(self, config_):
+#         toolkit.add_template_directory(config_, 'templates')
+#         toolkit.add_public_directory(config_, 'public')
+#         toolkit.add_resource('fanstatic',
+#             'my_hello_world')
+
+
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 
 class MyHelloWorldPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IRoutes)
 
     # IConfigurer
-
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-            'my_hello_world')
+        toolkit.add_resource('fanstatic', 'my_hello_world')
+
+    # IRoutes
+    def before_map(self, map):
+        # Tambahkan rute baru
+        map.connect('hello_world', '/hello', controller='ckanext.my_hello_world.controller:HelloWorldController', action='index')
+        return map
 
 
-# from ckan.plugins import SingletonPlugin, implements
-# from ckan.plugins.toolkit import IActions, response_json
-
-# class HelloWorldPlugin(SingletonPlugin):
-#     """
-#     Plugin sederhana untuk menambahkan API baru bernama 'hello_world'
-#     """
-
-#     implements(IActions)
-
-#     def get_actions(self):
-#         # Tambahkan aksi API baru bernama 'hello_world'
-#         return {
-#             'hello_world': hello_world
-#         }
-
-# def hello_world(context, data_dict):
-#     """
-#     Aksi API sederhana yang mengembalikan pesan
-#     """
-#     # Ambil parameter 'name' jika ada
-#     name = data_dict.get('name', 'World')
-#     return {'message': f'Hello, {name}!'}
+class HelloWorldController(toolkit.BaseController):
+    def index(self):
+        return "Hello, World!"
