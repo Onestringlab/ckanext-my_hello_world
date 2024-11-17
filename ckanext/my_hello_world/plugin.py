8a032b0247df279d71cb1a23d10969fb6d8f3093
yulrio <33221012@std.stei.itb.ac.id>
2024-11-17 13:27:19 +0700

    API Hello World

--- ckanext/my_hello_world/plugin.py
+++ ckanext/my_hello_world/plugin.py
@@ -1,14 +1,38 @@
-import ckan.plugins as plugins
-import ckan.plugins.toolkit as toolkit
+# import ckan.plugins as plugins
+# import ckan.plugins.toolkit as toolkit
 
 
-class MyHelloWorldPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
+# class MyHelloWorldPlugin(plugins.SingletonPlugin):
+#     plugins.implements(plugins.IConfigurer)
 
-    # IConfigurer
+#     # IConfigurer
 
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-            'my_hello_world')
+#     def update_config(self, config_):
+#         toolkit.add_template_directory(config_, 'templates')
+#         toolkit.add_public_directory(config_, 'public')
+#         toolkit.add_resource('fanstatic',
+#             'my_hello_world')
+
+
+from ckan.plugins import SingletonPlugin, implements
+from ckan.plugins import toolkit
+
+class HelloWorldPlugin(SingletonPlugin):
+    """
+    Plugin sederhana untuk menambahkan API baru bernama 'hello_world'
+    """
+    implements(toolkit.IActions)
+
+    def get_actions(self):
+        # Tambahkan aksi API baru bernama 'hello_world'
+        return {
+            'hello_world': hello_world
+        }
+
+def hello_world(context, data_dict):
+    """
+    Aksi API sederhana yang mengembalikan pesan
+    """
+    # Ambil parameter 'name' jika ada
+    name = data_dict.get('name', 'World')
+    return {'message': f'Hello, {name}!'}
