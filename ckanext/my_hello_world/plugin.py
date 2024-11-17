08ec552270dfe419b62aa6ac45e2a3cc8330e792
yulrio <33221012@std.stei.itb.ac.id>
2024-11-17 21:47:44 +0700

    Update message

--- ckanext/my_hello_world/plugin.py
+++ ckanext/my_hello_world/plugin.py
@@ -39,5 +39,8 @@ def hello_world_action(context, data_dict):
     """
     Fungsi ini akan menangani permintaan ke /api/3/action/hello_world
     """
+    # Cek apakah permintaan adalah GET
     if toolkit.request.method == 'GET':
         return {'message': 'Hello, World! This is a GET request!', 'success': True}
+    else:
+        raise toolkit.ValidationError('Only GET method is allowed for this endpoint.')
