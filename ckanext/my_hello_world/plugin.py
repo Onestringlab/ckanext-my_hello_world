c471f383995786d8aafad86abc174eee3954f6d2
yulrio <33221012@std.stei.itb.ac.id>
2024-11-18 21:11:02 +0700

    Initial Commit

--- ckanext/my_hello_world/plugin.py
+++ ckanext/my_hello_world/plugin.py
@@ -17,6 +17,7 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 from ckan.model import Package
+from ckan.model.meta import Session
 
 class MyHelloWorldPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
@@ -45,19 +46,23 @@ def hello_world_action(context, data_dict):
 
 def get_packages_action(context, data_dict):
     """
-    Mengambil data paket dari database
+    Mengambil data paket dari database menggunakan SQLAlchemy
     """
     try:
-        # Mendapatkan semua paket dari tabel `package`
-        packages = Package.get_all()
+        # Query semua paket dari tabel package
+        packages = Session.query(Package).all()
+        
+        # Mapping hasil query ke dalam format JSON-friendly
         package_list = [
             {
                 'id': pkg.id,
                 'name': pkg.name,
-                'title': pkg.title
+                'title': pkg.title,
+                'private': pkg.private
             }
             for pkg in packages
         ]
+        
         return {'success': True, 'data': package_list}
     except Exception as e:
         raise toolkit.ValidationError(f'Error fetching data: {str(e)}')
