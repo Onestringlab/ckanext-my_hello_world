0b5cd627129fb910ae36e659d433ae2011635b3d
yulrio <33221012@std.stei.itb.ac.id>
2024-11-21 10:30:35 +0700

    Update blueprint

--- ckanext/my_hello_world/plugin.py
+++ ckanext/my_hello_world/plugin.py
@@ -16,6 +16,7 @@
 
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.plugins import IRoutes
 from ckan.model import Package
 from ckan.model.meta import Session
 from flask import Blueprint, jsonify
